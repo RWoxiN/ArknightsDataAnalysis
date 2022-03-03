5bf4d390a8db28c69a6cb2522e7e63cf1e79dd27
Rian Ng <rian.ng@icloud.com>
2022-03-03 18:28:45 +0800

    新增主页

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask import render_template
 
 from api import *
@@ -6,12 +6,23 @@ from api import *
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<p>Arknights Data Analysis</p>'
+def index():
+    a_config = ada_config()
+    accounts_config = a_config.load_config_accounts()
+    accs_info = []
+    for acc_config in accounts_config:
+        a_api = ada_api(acc_config['token'], only_read=True)
+        acc_info = a_api.get_account_info()
+        accs_info.append(acc_info)
+    return render_template('index.html', accounts=accs_info)
 
-@app.route('/analyze/')
+@app.route('/analyze/<token>/refresh')
+def refresh_ada(token):
+    a_api = ada_api(token)
+    return redirect('/analyze/{}'.format(token))
+
 @app.route('/analyze/<token>')
-def analyze_results(token=None):
+def analyze_results(token):
     a_config = ada_config()
     accounts_config = a_config.load_config_accounts()
     accs_info = []
@@ -21,7 +32,6 @@ def analyze_results(token=None):
         accs_info.append(acc_info)
     a_api = ada_api(token, only_read=True)
     a_info = a_api.get_all_info()
-    print(a_info)
     return render_template('analysis.html', info=a_info, accounts=accs_info)
 
 if __name__ == '__main__':
